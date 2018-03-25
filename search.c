40d5290adb5ed6a5ba866410a55959b20af69c3d
robby1212 <sarvis.server1@gmail.com>
2018-03-25 18:24:54 -0500

    Add files via upload

--- search.c
+++ search.c
@@ -4,33 +4,64 @@
 #define LENGTH 180
 int main()
 {
-    char string[SIZE][LENGTH] = {"Test\0","These are times\0","That try Men's souls\0"};
+    char str[SIZE][LENGTH] = {"Test\0","These are times\0","That try Men's souls\0"};
 
     char mesg[]="Enter a string: ";		
-    char str[80];
+    char sub[80];
     int row,col;
-	int sizeofinput = strlen(str);	
-		
-
 		 
     initscr();				
     getmaxyx(stdscr,row,col);		
     mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
                      		
-    getstr(str);
-    mvprintw(LINES - 3, 0, "You Entered: %s", str);
+    getstr(sub);
+    mvprintw(LINES - 6, 0, "You Entered: %s", sub);
 
-    mvprintw(LINES - 4, 0, "Input Length: %i", sizeofinput);	
+    int i, j=0, k;
+   bool flag = true;
 
-    for (int i = 0; i < SIZE; ++i)
+   for ( int n = 0; n < SIZE; ++n )
     {
-        for (int n = 0; n < LENGTH; ++n)
+        j=0;
+      for(i=0; str[n][i]; i++)
+      {
+        if(str[n][i] == sub[j])
         {
-            mvprintw(LINES - 10, 0, "Testing:");
-            if (string[i][n] == str[0])
+          for(k=i, j=0; str[n][k] && sub[j]; j++, k++)
+            if(str[n][k]!=sub[j])
+                break;
+           if(!sub[j])
             {
-                mvprintw(LINES - 9, 0, "First Character Found");  
+            flag = false;
+            mvprintw(LINES - 5, 0, "Substring");
+            break;
             }
+        }
+      }
+    }
+    if ( flag )
+    {
+        mvprintw(LINES - 5, 0, "Not a substring");
+    }
+
+    if ( !flag )
+    {
+        char answer;
+        mvprintw(LINES - 4, 0, "Do you want to replace the string located here with another? y/n: ");
+        answer = getch();
+
+        mvprintw(LINES - 10, 0, "Here: %c", answer);
+        
+        if ( &answer == "y" || &answer == "Y" )
+        {
+            mvprintw(LINES - 3, 0, "What do you want to replace it with?");
+            char newstr[80];
+            getstr(newstr);
+            mvprintw(LINES - 3, 0, "You Entered: %s", newstr);
+        }
+        else
+        {
+            mvprintw(LINES - 3, 0, "Thsi is stupid");
         }
     }
 
