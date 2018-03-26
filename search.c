4c6f55f05aa6e46ee85154a9ca79fe24a32b41c3
robby1212 <sarvis.server1@gmail.com>
2018-03-25 22:57:41 -0500

    Add files via upload

--- search.c
+++ search.c
@@ -1,33 +1,16 @@
 #include <ncurses.h>			
 #include <string.h> 
+#include "search.h"
 #define SIZE 10
 #define LENGTH 180
-
-
-
-
-int main()
-{
-    char str[SIZE][LENGTH] = {"Test\0","These are times\0","That try Men's souls\0"};
-
-    char mesg[]="Enter a string: ";		
-    char sub[80];
-    int row,col;
-
-    int linelocation;
-    int whichstring;
-		 
-    initscr();				
-    getmaxyx(stdscr,row,col);	
-	
+void search(*str)
+{	
     mvprintw(LINES - 4, 0, "%s",mesg);              		
     getstr(sub);
 
     mvprintw(LINES - 4, 0, "You Entered: %s", sub);
 
-    int i, j=0, k;
-   bool flag = true;
-   for ( int n = 0; n < SIZE; ++n )
+    for ( int n = 0; n < SIZE; ++n )
     {
         j=0;
       for(i=0; str[n][i]; i++)
@@ -55,25 +38,17 @@ int main()
 
     if ( !flag )
     {
-        char answer;
         mvprintw(LINES - 3, 0, "Do you want to replace the string located here with another? y/n: ");
         answer = getch();
         
         if ( answer == 'y' || answer == 'Y' )
         {
-            mvprintw(LINES - 2, 0, "What do you want to replace it with?");
-            char newstr[80];
+            mvprintw(LINES - 2, 0, "Replacement: ");
             getstr(newstr);
-            strcpy(str[whichstring][linelocation], newstr);
-            
-        }
-        else
-        {
-            mvprintw(LINES - 2, 0, "Thsi is stupid");
+            char *p = &newstr;
+            char *h = &str[whichstring][linelocation];
+            strcpy(h, p); 
+            mvprintw(LINES -1, 0, "Completed");           
         }
     }
-
-    getch();
-    endwin();
-    return 0;
 }
---
+++ search.h
@@ -0,0 +1,28 @@
+// search.h
+// Programmed by Robert Sarvis
+
+#ifndef search_h
+#define buffer_h
+
+#include <ncurses.h>			
+#include <string.h>
+ 
+#define SIZE 50
+#define LENGTH 100
+
+struct search;
+
+typedef struct search
+{
+    char mesg[]="Enter a string: ";		
+    char sub[80];
+    int row,col;
+
+    int linelocation;
+    int whichstring;
+
+    int i, j=0, k;
+    bool flag = true;
+
+    char answer;
+    char newstr[80];
