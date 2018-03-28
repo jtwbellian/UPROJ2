7b580b1153d718a86dfad0764c19758b9b9ccd63
jbarulich <35403092+jbarulich@users.noreply.github.com>
2018-03-28 06:23:49 -0500

    Update search.c

--- search.c
+++ search.c
@@ -51,3 +51,74 @@ void search(char **str)
         }
     }
 }
+
+
+/*
+#include "search.h"
+#include <ncurses.h>			
+#include <string.h>
+#include <ctype.h> 
+
+struct found{
+	int y; //row
+	int x; //col
+};
+
+void search(char **str, int lineCount)
+{	
+   	
+	struct found whereAndLength[SIZE];
+
+   	mvprintw(LINES - 4, 0, "%s",mesg);              		
+	getstr(sub);
+	mvprintw(LINES - 4, 0, "You Entered: %s", sub);
+	while (strlen(sub) <= 0) {
+		mvprintw(LINES - 4, 0, "%s", mesg);
+	}
+    
+	int subStrLen = strlen(sub); //Length of the searched term
+	char* stringPtr; //Points to the entire row
+	int countMatch = 0; //Count character matches
+	int totalMatch = 0; //Count total matches of searched term
+	
+	//Loop for all the lines of the file
+	for (int i = 0; i < lineCount; i++){
+		
+        stringPtr = str[lineCount];
+		int strLen = strlen(stringPtr);
+		
+        //Loop to check each character of the specific line
+		for (int j = 0; j < strLen; j++){
+			if (stringPtr[j] == sub[0]){
+				
+                //Loop to see if the subsequent characters of possible match is actually a match
+				//Increment countMatch for each consecutive character 
+				countMatch++;
+				for (int k = j + 1, n = 1; k < j + subStrLen && k < strLen && n < subStrLen; k++, n++){
+					if (stringPtr[k] == sub[n]){
+						countMatch++;
+					}
+				}
+                
+                //A total match was found, set the values in the array
+				if (countMatch == subStrLen){
+					whereAndLength[totalMatch].y = i;
+					whereAndLength[totalMatch].x = j;
+					totalMatch++;
+				}
+                
+                //Resets for the next character set
+				countMatch = 0;
+			}
+		
+		}
+		
+	}
+    //If totalMatch == 0, print out "nothing found"
+    //Else
+    //print out "replacement: ", get the replacement string
+    //Loop through the array from 0 to totalMatch - 1
+    //Use row, column, and subStrLen to set the replacement where the original searched term was found
+    
+}
+*/
