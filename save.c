40f0c4fbe269c0311e67ddabdab0685f0bdcf591
James Bellian <32314548+jtwbellian@users.noreply.github.com>
2018-03-29 03:06:27 -0500

    Saves to multiple lines now

--- save.c
+++ save.c
@@ -1,3 +1,6 @@
+// save.c 
+// Programmed by Derek Sittenauer
+
  #include <stdio.h>
  #include <stdlib.h>
  #include <ncurses.h>
@@ -11,7 +14,9 @@ void save(char * filename, int num_lines, char ** text, int h, int w) {
 	   for (int j = 0; j < strlen(text[i]); j++) {
 			fputc(text[i][j], file);
 		}
+	fprintf(file, "\r\n");
    }
    mvprintw(h/2+1, w/3,"File saved");
-   fclose(filename);
+   fclose(file);
  }
+
