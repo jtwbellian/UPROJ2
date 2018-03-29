fad2c23dc6d84e68d3315c9c29ac469df91ceca2
DerekSittenauer <dcs78@txstate.edu>
2018-03-29 11:22:18 -0500

    Add files via upload

--- copy.c
+++ copy.c
@@ -12,10 +12,9 @@ void copyStart() {
 	//get cursor starting position
 	getyx(stdscr, tempy, tempx);
 	mvprintw(0, 0, "%s", "Starting index chosen.");
-	refresh();
 	move(tempy, tempx);
 	getyx(stdscr, starty, startx);
-	   
+   
 }
 
 
@@ -26,15 +25,14 @@ void copyEnd() {
 	getyx(stdscr, tempy, tempx);
 	
 	mvprintw(0, 0, "%s", "End index chosen.");
-	refresh();
 	move(tempy, tempx);
 }
 
-void paste() {
+char* paste() {
 	
 	int pastex, pastey;
 	char copyString[endx-startx]; //string to store copied text
-	
+
 	//load copied text into string
 	for (int s = startx; s < endx; s++) {
 		copyString[s] = mvinch(starty, s);
@@ -44,6 +42,8 @@ void paste() {
 	getyx(stdscr, pastey, pastex);
 	for (int i = 0; i < endx - startx; i++) {
 		mvprintw(pastey, i + pastex, "%c", copyString[i]);
-	} refresh();
+	}
+
+	return copyString;
 	
 }
