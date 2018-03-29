1b2f9d1d8425a48cd89208013ee58abedf61c485
DerekSittenauer <dcs78@txstate.edu>
2018-03-28 23:51:31 -0500

    Add files via upload

--- save.c
+++ save.c
@@ -3,14 +3,15 @@
  #include <ncurses.h>
  #include "save.h"
 
-void save(char *filename, int num_lines, char ** text) {
+void save(char * filename, int num_lines, char ** text, int h, int w) {
    FILE *file = fopen(filename, "w");
    
    for (int i = 0; i < num_lines; i++) {
 	
-	   for (int j = 0; j < strLen(text[i]); j++) {
+	   for (int j = 0; j < strlen(text[i]); j++) {
 			fputc(text[i][j], file);
 		}
    }
+   mvprintw(h/2+1, w/3,"File saved");
    fclose(filename);
  }
