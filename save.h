568ae17c82c5a46bb850189c07fdae3c7547591e
DerekSittenauer <dcs78@txstate.edu>
2018-03-28 23:08:31 -0500

    Add files via upload

--- save.h
+++ save.h
@@ -1,19 +1,6 @@
-#ifndef save_h
-#define save_h
-#include <ctype.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <termios.h>
-#include <time.h>
-#include <unistd.h>
+#ifndef SAVE
+#define SAVE
 
-
-char *editorRowsToString(int *buflen, int num_lines, char ** text);
-
-void editorSave(string filename, int num_lines, char ** text)
+void save(char *filename, int num_lines, char ** text);
 
 #endif
